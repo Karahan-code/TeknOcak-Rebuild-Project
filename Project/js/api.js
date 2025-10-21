f956552ac6e017b901683ccc1694223ab0011353
Karahan-code <onurbey0603@hotmail.com>
2025-10-21 20:15:51 +0300

    Genel Navbar Düzeni
    1-Navbar düzeni yapıldı.
    2-Navbar responsive uyarlandı.
    3-Kullanıcı giriş çıkış simulasyonu yapıldı (api.js)
    4- Geçici renkler atıldı.

--- Project/js/api.js
+++ Project/js/api.js
@@ -1,1 +1,14 @@
 //Backend API çağrıları burada olacak
+
+// Deneme amaçlı: giriş yapıldığında değişiyor
+let isLoggedIn = true; 
+
+function updateNavbar() {
+  if (isLoggedIn) {
+    document.body.classList.add("logged-in");
+  } else {
+    document.body.classList.remove("logged-in");
+  }
+}
+// İlk yüklemede kontrol et
+updateNavbar();
--- Project/js/ui.js
+++ Project/js/ui.js
@@ -1,1 +1,20 @@
 //Arayüzle ilgili JS
+
+    //Navbar
+
+    //Navbar açma buttonu
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+})
+
+    //Navbar kullanıcı ekranı Dropdown Click
+const dropdownToggle = document.getElementById('userbtn');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+dropdownToggle.addEventListener('click',() => {
+    dropdownMenu.classList.toggle("dropdown-menu-show");
+})
