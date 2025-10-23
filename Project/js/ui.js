0d80901a6b2d618896c331b45b0b1d555a51a906
Karahan-code <onurbey0603@hotmail.com>
2025-10-23 18:09:57 +0300

    Büyük Değişiklikler
    Eklemeler yapıldı
    Renk paleti değişti
    Arkaplan'a ekelemeler yapıldı
    Navbar düzenlemesi yapıldı
    Yeni içerik temeli atıldı

--- Project/js/ui.js
+++ Project/js/ui.js
@@ -1,8 +1,8 @@
 //Arayüzle ilgili JS
 
-    //Navbar
+//Navbar
 
-    //Navbar açma buttonu
+//Navbar açma buttonu
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar-menu');
 
@@ -11,10 +11,41 @@ navbarToggle.addEventListener('click', () => {
     navbarMenu.classList.toggle('active');
 })
 
-    //Navbar kullanıcı ekranı Dropdown Click
+//Navbar kullanıcı ekranı Dropdown Click
 const dropdownToggle = document.getElementById('userbtn');
 const dropdownMenu = document.getElementById('dropdownMenu');
 
-dropdownToggle.addEventListener('click',() => {
+dropdownToggle.addEventListener('click', () => {
     dropdownMenu.classList.toggle("dropdown-menu-show");
 })
+
+//Carousel Slider
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
